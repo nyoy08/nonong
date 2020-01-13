7163dcc9dd85edbc58958a6ea9ae2a28a3923df1
nyoy08 <nazariotorio9@gmail.com>
2020-01-13 20:31:41 +0800

    Add files via upload

---
+++ miner-cpuchain.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+#       stratum-ru.rplant.xyz   /Moscow/
+#       stratum-eu.rplant.xyz   /London/
+#       stratum-asia.rplant.xyz /Singapore/
+#
+while [ 1 ]; do
+./cpuminer-sse2 -a cpupower -o stratum+tcp://stratum-ru.rplant.xyz:7029 -u Cb8zvEzh15QWLiNKr2vEB83trJwUGqavAR
+sleep 0
+done
